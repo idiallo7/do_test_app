0bda94e454067d5261d1c2a1ade8047a49cfdcad
idiallo7 <idiallo@gwu.edu>
2020-10-26 19:01:32 -0400

    Fixed Allowed_host

--- do_app_test/settings.py
+++ do_app_test/settings.py
@@ -28,10 +28,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', "False") == "True"
-#DEBUG = os.environ.get('DEBUG_VALUE', False)
+#DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1, localhost').split(',')
-
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # Application definition
 
