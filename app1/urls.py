34a1585a9fc2741da24c43d6b71e85c4b6f796ad
idiallo7 <idiallo@gwu.edu>
2020-10-26 19:13:20 -0400

    Features page

--- app1/urls.py
+++ app1/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('features', views.features, name='features')
 ]
--- app1/views.py
+++ app1/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def home(request):
     context={'home_res':'HOME_RES'}
     return render(request, 'app1/home.html', context)
+
+
+def features(request):
+    context={'features_res':'FEATURES_RES'}
+    return render(request, 'app1/features.html', context)
